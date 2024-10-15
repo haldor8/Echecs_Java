a48517a78aadc0479a95e971879cad7d843103db
Charles <charles-iacopino@hotmail.com>
2024-10-15 09:10:06 +0200

    Modification type variable client dans plateau

--- src/Plateau.java
+++ src/Plateau.java
@@ -1,37 +1,38 @@
 import java.io.ObjectOutputStream;
 
 import javax.swing.*;
+import java.awt.*;
 
 // Se situe du côté client et gère l'envoi d'informations au serveur
 public class Plateau extends JPanel {
 
     private Pieces[][] matrice;
-    private Client client;
+    private Color client;
     // private ObjectOutputStream sortie;
-    // private int style;
+    // private int style; // Si on souhaite faire plusieurs styles de plateau
     private int nb_lignes, nb_colonnes;
 
-    public Plateau(Client le_client) {
+    public Plateau(Color le_client) {
         // Instancier tous les pions et leurs positions de base dans le constructeur
         this.matrice = new Pieces[nb_lignes][nb_colonnes];
         this.client = le_client;
 
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < nb_colonnes; i++) {
             this.matrice[1][i] = new Pion(); // Placer les pions blancs
             this.matrice[6][i] = new Pion(); // Placer les pions noirs
         }
 
-        for (int i = 0; i < 8; i += 7) {
+        for (int i = 0; i < nb_colonnes; i += 7) {
             this.matrice[0][i] = new Tour(); // Placer les tours blanches
             this.matrice[7][i] = new Tour(); // Placer les tours noires
         }
 
-        for (int i = 1; i < 8; i += 5) {
+        for (int i = 1; i < nb_colonnes; i += 5) {
             this.matrice[0][i] = new Cavalier(); // Placer les cavaliers blancs
             this.matrice[7][i] = new Cavalier(); // Placer les cavaliers noirs
         }
 
-        for (int i = 2; i < 8; i += 3) {
+        for (int i = 2; i < nb_colonnes; i += 3) {
             this.matrice[0][i] = new Fou(); // Placer les fous blancs
             this.matrice[7][i] = new Fou(); // Placer les fous noirs
         }
@@ -41,9 +42,12 @@ public class Plateau extends JPanel {
 
         this.matrice[0][4] = new Roi(); // Placer la reine blanche
         this.matrice[7][4] = new Roi(); // Placer la reine noire
+
+        // Combler le plateau avec des cases vides ou des pions vides jsp
     }
 
-    // Ne pas oublier d'inverser les coordonnées lorsque les pions noirs jouent
+    // Ne pas oublier d'inverser les coordonnées reçues par l'écouteur lorsque les
+    // pions noirs jouent
 
     // Implémenter le 1 click highlight et le drag&drop
 }
