c262d76116d717d2d4ae89121d9dd859d55b338a
unknown <maxalexmart59@gmail.com>
2024-10-15 09:40:57 +0200

    ajout des classes pion, fou, reine, roi, cavaliler, tour

--- src/Roi.java
+++ src/Roi.java
@@ -1,3 +1,19 @@
 public class Roi extends Pieces {
-    
+
+    public Roi(int num_ligne, int num_colonne, Client proprietaire, boolean a_bouge, List<String> liste_deplacement, Image icon, boolean est_blanc) {
+        super(num_ligne, num_colonne, proprietaire, a_bouge, liste_deplacement, icon, est_blanc);
+    }
+
+    @Override
+    public boolean deplacement_valide(int x_initial, int y_initial, int x_final, int y_final, Pieces[][] echiquier) {
+        int delta_x = Math.abs(x_final - x_initial);
+        int delta_y = Math.abs(y_final - y_initial);
+
+        if (echiquier[x_final][y_final] != null && echiquier[x_final][y_final].get_proprietaire().equals(this.get_proprietaire())) {
+            return false;
+        }
+
+        return delta_x <= 1 && delta_y <= 1;
+    }
 }
+
