3db6e57513adf34a1889981cadc436ce30be5b69
Charles <charles-iacopino@hotmail.com>
2025-01-04 15:34:36 +0100

    Cleanup du main

--- src/Pieces.java
+++ src/Pieces.java
@@ -1,201 +1,73 @@
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.io.ObjectOutputStream;
-
-import javax.swing.*;
 import java.awt.*;
+import java.io.Serializable;
+import javax.swing.*;
 import java.util.*;
+import org.apache.batik.transcoder.*;
+import org.apache.batik.transcoder.image.*;
 
-public class Plateau extends JPanel {
-    private Pieces[][] matrice;
-    private JPanel[][] cases_graphiques;
-    private ObjectOutputStream sortie;
-    private int id_joueur;
-    public static int nb_lignes = 8;
-    public static int nb_colonnes = 8;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.List;
 
-    private static int DISTANCE = 50;
 
-    private int ligne_buffer = -1;
-    private int colonne_buffer = -1;
+// Décris les informations des pièces
+public class Pieces implements Serializable {
+    private int proprietaire;
+    private ImageIcon icon;
+    private int couleur; // 1 pour blanc, 2 pour noir
+    private String la_couleur; //noir ou blanc
 
-    public Plateau(int le_client) {
-        this.matrice = new Pieces[nb_lignes][nb_colonnes];
-        this.cases_graphiques = new JPanel[nb_lignes][nb_colonnes];
-        this.id_joueur = le_client;
-
-        this.setLayout(new GridLayout(nb_lignes, nb_colonnes));
-
-        // Initialiser le plateau graphique
-        for (int i = 0; i < nb_lignes; i++) {
-            for (int j = 0; j < nb_colonnes; j++) {
-                JPanel case_panel = new JPanel(new BorderLayout());
-                cases_graphiques[i][j] = case_panel;
-
-                if ((i + j) % 2 == 0) {
-                    case_panel.setBackground(new Color(238, 238, 210));
-                } else {
-                    case_panel.setBackground(new Color(118, 150, 86));
-                }
-
-                this.add(case_panel);
-            }
-        }
-
-        // Initialiser les pièces
-        initialiser_pieces();
-
-        // Mettre à jour l'affichage graphique
-        mettre_a_jour_affichage();
-
-        this.addMouseListener(new MouseAdapter(){
-            @Override
-            public void mousePressed(MouseEvent e){
-                int ligne = (int)(e.getX()/(getSize().getWidth()/8));
-                int colonne = (int)(e.getY()/(getSize().getHeight()/8));
-                System.out.println("Appuye en :\nX=" + ligne + " Y=" + colonne);
-
-                if(ligne_buffer == -1){
-                    if(matrice[ligne][colonne] != null) {
-                        if(matrice[ligne][colonne].get_proprietaire() == id_joueur){
-                            maj_buffer(ligne, colonne);
-                            System.out.println(matrice[ligne][colonne]);
-                            // afficher_deplacements();
-                        }
-                    }
-                }else{
-                    // Si la pièce déjà sélectionnée est différente de la pièce dans le buffer
-                    if(!matrice[ligne][colonne].equals(matrice[ligne_buffer][colonne_buffer])
-                            && matrice[ligne_buffer][colonne_buffer].get_proprietaire() != matrice[ligne][colonne].get_proprietaire()){
-                        // Vérifier
-                        // Envoyer le déplacement au serveur
-                        /*
-                        Deplacement depl = new Deplacement()
-                        try{
-                            sortie.writeObject(depl);
-                        }catch(IOException ex){
-                            // gérer
-                        }
-                         */
-                        // Flush le buffer
-                        clear_buffer();
-                    }
-                }
-            }
-
-            public void mouseReleased(MouseEvent e){
-                int ligne = (int)(e.getX()/(getSize().getWidth()/8));
-                int colonne = (int)(e.getY()/(getSize().getHeight()/8));
-                System.out.println("Relache en :\nX=" + ligne + " Y=" + colonne);
-                if(ligne_buffer != -1){
-                    if(ligne != ligne_buffer && colonne != colonne_buffer){
-                        // Vérifier
-                        // Envoyer
-                        clear_buffer();
-                    }
-                }
-            }
-        });
+    public Pieces(int proprietaire) {
+        this.proprietaire = proprietaire;
+        la_couleur = proprietaire == 1 ? "blanc" : "noir";
     }
 
-    private void initialiser_pieces() {
-        // Ajouter les pions
-        if (this.id_joueur == 2) {
-            for (int i = 0; i < nb_colonnes; i++) {
-                matrice[1][i] = new Pion(1);
-                matrice[6][i] = new Pion(2);
-            }
+    public int get_proprietaire() {
+        return proprietaire;
+    }
+    public int get_Couleur(){return couleur;}
+    //retourne la couleur (noir ou blanc, plutôt que le  int qui est utile pour certaines manips)
+    public String get_Couleur_String(){return la_couleur;}
 
-            // Ajouter les autres pièces
-            matrice[0][0] = new Tour(1);
-            matrice[0][7] = new Tour(1);
-            matrice[7][0] = new Tour(2);
-            matrice[7][7] = new Tour(2);
 
-            matrice[0][1] = new Cavalier(1);
-            matrice[0][6] = new Cavalier(1);
-            matrice[7][1] = new Cavalier(2);
-            matrice[7][6] = new Cavalier(2);
+    public ImageIcon get_icon() {
+        return icon;
+    }
 
-            matrice[0][2] = new Fou(1);
-            matrice[0][5] = new Fou(1);
-            matrice[7][2] = new Fou(2);
-            matrice[7][5] = new Fou(2);
+    public void chargerIcon(String cheminSVG) {
+        try {
+            // Transcodeur pour convertir SVG en BufferedImage
+            TranscoderInput input = new TranscoderInput(new File(cheminSVG).toURI().toString());
+            BufferedImageTranscoder transcoder = new BufferedImageTranscoder();
 
-            matrice[0][3] = new Reine(1);
-            matrice[0][4] = new Roi(1);
-            matrice[7][3] = new Reine(2);
-            matrice[7][4] = new Roi(2);
-        } else {
-            for (int i = 0; i < nb_colonnes; i++) {
-                matrice[1][i] = new Pion(2);
-                matrice[6][i] = new Pion(1);
-            }
-
-            // Ajouter les autres pièces
-            matrice[0][0] = new Tour(2);
-            matrice[0][7] = new Tour(2);
-            matrice[7][0] = new Tour(1);
-            matrice[7][7] = new Tour(1);
-
-            matrice[0][1] = new Cavalier(2);
-            matrice[0][6] = new Cavalier(2);
-            matrice[7][1] = new Cavalier(1);
-            matrice[7][6] = new Cavalier(1);
-
-            matrice[0][2] = new Fou(2);
-            matrice[0][5] = new Fou(2);
-            matrice[7][2] = new Fou(1);
-            matrice[7][5] = new Fou(1);
-
-            matrice[0][3] = new Reine(2);
-            matrice[0][4] = new Roi(2);
-            matrice[7][3] = new Reine(1);
-            matrice[7][4] = new Roi(1);
+            transcoder.transcode(input, null);
+            BufferedImage image = transcoder.getBufferedImage();
+            icon = new ImageIcon(image); // Convertir en ImageIcon
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    private void mettre_a_jour_affichage() {
-        for (int i = 0; i < nb_lignes; i++) {
-            for (int j = 0; j < nb_colonnes; j++) {
-                JPanel case_panel = cases_graphiques[i][j];
-                case_panel.removeAll();
+    private static class BufferedImageTranscoder extends ImageTranscoder {
+        private BufferedImage image;
 
-                Pieces piece = matrice[i][j];
-                if (piece != null && piece.get_icon() != null) {
-                    JLabel label = new JLabel(piece.get_icon());
-                    case_panel.add(label, BorderLayout.CENTER);
-                }
-
-                case_panel.revalidate();
-                case_panel.repaint();
-            }
+        @Override
+        public BufferedImage createImage(int width, int height) {
+            BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g2d = bufferedImage.createGraphics();
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g2d.dispose();
+            return bufferedImage;
         }
-    }
 
-    public void set_out(ObjectOutputStream la_sortie) {
-        this.sortie = la_sortie;
-    }
 
-    public static int getNb_lignes() {
-        return nb_lignes;
-    }
+        @Override
+        public void writeImage(BufferedImage image, TranscoderOutput output) {
+            this.image = image;
+        }
 
-    public static int getNb_colonnes() {
-        return nb_colonnes;
-    }
-
-    public void afficher_deplacements(int x, int y, Pieces la_piece){
-
-    }
-
-    private void maj_buffer(int x, int y){
-        this.ligne_buffer = x;
-        this.colonne_buffer = y;
-    }
-
-    private void clear_buffer(){
-        this.ligne_buffer = -1;
-        this.colonne_buffer = -1;
+        public BufferedImage getBufferedImage() {
+            return image;
+        }
     }
 }
